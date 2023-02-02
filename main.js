52ed8afd86c60d7edd3b0c743f72ad7620a18667
Juan Cruz Rodriguez <siwidjc@gmail.com>
2023-02-01 23:41:11 -0300

    PreProyecto 3 - Juan Cruz Rodriguez

--- main.js
+++ main.js
@@ -1,78 +1,60 @@
+// // Function to retrieve the cart from local storage
+// function getCart() {
+//   return JSON.parse(localStorage.getItem('cart')) || [];
+// }
 
-let cart = [];
+// // Function to save the cart to local storage
+// function saveCart(cart) {
+//   localStorage.setItem('cart', JSON.stringify(cart));
+// }
 
 let products = [];
+      let productId = 1;
 
-function addToCart(id) {
-  let product = products.find(function (product) {
-    return product.id === id;
-  });
+      function addProduct() {
+        let productDescription = document.getElementById("productDescription").value;
+        let productPrice = document.getElementById("productPrice").value;
 
-  cart.push(product);
+        let existingProduct = products.find(product => product.description === productDescription);
+        if (existingProduct) {
+          existingProduct.quantity++;
+        } else {
+          let product = {
+            id: productId,
+            description: productDescription,
+            price: productPrice,
+            quantity: 1
+          };
 
-  alert("Producto agregado al carrito!");
-}
-function addProduct() {
-  let id = prompt("Ingresa el id del producto:");
-  let description = prompt("Ingresá la descripción del producto:");
-  let price = Number(prompt("Ingresá el precio del producto:"));
+          products.push(product);
+          productId++;
+        }
 
-  let product = {
-    id: id,
-    description: description,
-    price: price
-  };
-  products.push(product);
-
-  alert("Producto agregado!");
-}
-
-while (true) {
-  let menu = "1. Ver productos\n2. Agregar producto\n3. Agregar al carrito\n4. Ver carrito\n5. Salir\nIngresá tu opción:";
-  let choice = prompt(menu);
-  if (choice === "1") {
-    for (let i = 0; i < products.length; i++) {
-      let product = products[i];
-      alert(`${product.id}: ${product.description} - $${product.price}`);
-    }
-  } else if (choice === "2") {
-    if (products.length === 8) {
-      alert('Tu carrito está lleno! Por favor borra algunos productos antes de agregar más')
-    } else {
-      addProduct();
-    }
-  } else if (choice === "3") {
-    if (cart.length === 8) {
-      alert("Tu carrito está lleno! Por favor borra algunos productos antes de agregar más");
-    } else {
-      let id = prompt("Ingresá el id del producto que deseas agregar al carrito:");
-      let product = products.find(product => {
-        return product.id === id;
-      })
-      cart.push(product);
-      alert('Producto agregado al carrito!');
-    }
-  } else if (choice === "4") {
-    if (cart.length === 0) {
-      alert("Tu carrito está vacío!");
-    } else {
-      let total = 0;
-      let productDescriptions = [];
-
-      for (let i = 0; i < cart.length; i++) {
-        let item = cart[i];
-        total += item.price;
-
-        productDescriptions.push(`${item.id}: ${item.description} - $${item.price}`);
+        updateProducts();
+        updateCart();
       }
 
-      let cartContents = productDescriptions.join("\n");
+      function updateProducts() {
+        let productsDiv = document.getElementById("products");
+        productsDiv.innerHTML = "";
 
-      alert(`Contenidos del carrito:\n${cartContents}\nTotal: $${total}`);
-    }
-  } else if (choice === "5") {
-    break;
-  } else {
-    alert("Opción inválida, por favor ingrese una opción válida");
-  }
-}
+        for (let i = 0; i < products.length; i++) {
+          let product = products[i];
+          productsDiv.innerHTML += product.description + " - $" + product.price + "<br>";
+        }
+      }
+
+      function updateCart() {
+        let cartDiv = document.getElementById("cart");
+        cartDiv.innerHTML = "";
+
+        let total = 0;
+        for (let i = 0; i < products.length; i++) {
+          let product = products[i];
+          cartDiv.innerHTML +=
+           "(" + product.id + ")" + product.description + " - $" + product.price + " x " + product.quantity + "<br>";
+          total += product.price * product.quantity;
+        }
+
+        cartDiv.innerHTML += "Total: $" + total;
+      }
